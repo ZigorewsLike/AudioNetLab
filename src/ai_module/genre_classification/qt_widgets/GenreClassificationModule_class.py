EQ setup buttons, test auto eq control and fix predict graphs (gradient)

src/ai_module/genre_classification/qt_widgets/GenreClassificationModule_class.py
@@ -21,7 +21,8 @@ from src.core.log_system import print_d, print_e, print_i
 from src.core.workers import GenrePredictWorker
 from src.function_lib.math_lib import median
 from src.function_lib.ai import load_sess_model
-from src.core.settings.qt_widgets.SettingsEQWidget_class import EQWidget
+from src.core.qt_widgets import EQWidget
+from src.enums import EQType
 
 if not ONNX_INFERENCE:
     try:
@@ -74,10 +75,12 @@ class GenreClassifierModule(QWidget):
         self.genre_gradient = QLinearGradient(0, 0, self.width(), 0)
         self.genre_gradient.setColorAt(0.0, QColor("#2A2A2A"))
         self.genre_gradient.setColorAt(1.0, QColor("#2A2A2A"))
-        self.global_results = []
+        self.global_results: Optional[list] = None
         self.drawing_text_pos: Optional[QPoint] = None
 
-        self.eq = EQWidget(self)
+        self.eq = EQWidget(EQType.ACTIVE, self)
+        self.genre_eq = np.random.rand(8, 20)
+        self.current_genre: str = ""
 
         self.genre_dict: Dict[int, str] = {
             0: "Electronic",
@@ -125,12 +128,23 @@ class GenreClassifierModule(QWidget):
 
     def resizeEvent(self, event: QResizeEvent) -> None:
         super().resizeEvent(event)
-        if self.global_results:
-            self.genre_gradient = QLinearGradient(0, 0, self.width(), 0)
+        self.recalc_sizes()
 
-            for step in range(len(self.global_results)):
-                self.genre_gradient.setColorAt(step / len(self.global_results),
-                                               QColor(self.genre_color[self.global_results[step]]))
+    def set_gradient_color(self, genre_result: List[int]) -> None:
+        self.genre_gradient = QLinearGradient(0, 0, self.width(), 0)
+        sample_width = self.mf.audio_player.sample_rate * 3
+        for step, result in enumerate(genre_result):
+            self.genre_gradient.setColorAt(
+                (step * sample_width + sample_width / 2) / self.mf.audio_player.waveform.shape[0],
+                QColor(self.genre_color[result]))
+            self.genre_gradient.setColorAt(
+                ((step + 1) * sample_width) / self.mf.audio_player.waveform.shape[0],
+                QColor(self.genre_color[result]))
+
+
+    def recalc_sizes(self) -> None:
+        if self.global_results:
+            self.set_gradient_color(self.global_results)
         self.eq.move(int(self.width() / 2 - self.eq.width() / 2),
                      self.best_of_label.y() + self.best_of_label.height() + 10)
         self.update()
@@ -140,12 +154,9 @@ class GenreClassifierModule(QWidget):
         if not out:
             # TODO: Сообщить о том, что выход пустой
             return
-        self.genre_gradient = QLinearGradient(0, 0, self.width(), 0)
-        sample_len: int = math.ceil(self.mf.audio_player.waveform.shape[0] / self.mf.audio_player.sample_rate / 3)
-        for step in range(sample_len):
-            self.genre_gradient.setColorAt(step / sample_len, QColor(self.genre_color[out[step]]))
-
+        self.set_gradient_color(out)
         self.global_results = out
+        print_d(out)
         counts = np.bincount(np.array(out))
 
         best_of_text: str = ""
@@ -179,20 +190,20 @@ class GenreClassifierModule(QWidget):
         if self.isVisible():
             start_time: float = time.time()
             painter = QPainter(self)
-            painter.fillRect(10, self.graph_y, self.width() - 21, self.graph_height, QBrush(self.genre_gradient))
+            painter.fillRect(0, self.graph_y, self.width(), self.graph_height, QBrush(self.genre_gradient))
 
             painter.setPen(QPen(QColor("#FA6900"), 1.0, Qt.PenStyle.DashLine))
-            cursor_x: int = round(self.cursor_position * (self.width() - 21) + 10)
+            cursor_x: int = round(self.cursor_position * (self.width()))
             painter.drawLine(cursor_x, self.graph_y, cursor_x, self.graph_y + self.graph_height)
 
-            if self.drawing_text_pos is not None and len(self.global_results) > 0:
+            if self.drawing_text_pos is not None and self.global_results is not None and len(self.global_results) > 0:
                 painter.setPen(QPen(Qt.GlobalColor.white, 1.0, Qt.PenStyle.DashLine))
                 painter.setFont(QFont("Arial", 14))
                 painter.drawLine(self.drawing_text_pos.x(), self.graph_y, self.drawing_text_pos.x(),
                                  self.graph_y + self.graph_height)
 
                 # painter.setCompositionMode(QPainter.CompositionMode.RasterOp_SourceXorDestination)
-                index_factor: float = (self.drawing_text_pos.x() - 10) / (self.width() - 21)
+                index_factor: float = (self.drawing_text_pos.x()) / (self.width())
                 index: int = median(0, round((len(self.global_results) - 1) * index_factor), len(self.global_results) - 1)
                 genre_text: str = self.genre_dict[self.global_results[index]]
                 genre_text_width: int = painter.fontMetrics().boundingRect(genre_text).width()
@@ -201,10 +212,10 @@ class GenreClassifierModule(QWidget):
             elif self.global_results:
                 painter.setPen(QPen(Qt.GlobalColor.white, 1.0, Qt.PenStyle.DashLine))
                 painter.setFont(QFont("Arial", 14))
-                index: int = median(0, round((len(self.global_results) - 1) * self.cursor_position), len(self.global_results) - 1)
-                genre_text: str = self.genre_dict[self.global_results[index]]
-                genre_text_width: int = painter.fontMetrics().boundingRect(genre_text).width()
-                painter.drawText(int(cursor_x - genre_text_width / 2), self.graph_y - 10, genre_text)
+                # index: int = median(0, round((len(self.global_results) - 1) * self.cursor_position), len(self.global_results) - 1)
+                # genre_text: str = self.genre_dict[self.global_results[index]]
+                genre_text_width: int = painter.fontMetrics().boundingRect(self.current_genre).width()
+                painter.drawText(int(cursor_x - genre_text_width / 2), self.graph_y - 10, self.current_genre)
             if PROFILE:
                 self.mf.profiling.add_draw_time(self.__class__.__name__, time.time() - start_time)
 
@@ -277,9 +288,26 @@ class GenreClassifierModule(QWidget):
 
         wb.save('data/local/analysis.xlsx')
 
+    def reset_result(self) -> None:
+        self.global_results = None
+        self.genre_gradient = QLinearGradient(0, 0, self.width(), 0)
+        self.genre_gradient.setColorAt(0.0, QColor("#2A2A2A"))
+        self.genre_gradient.setColorAt(1.0, QColor("#2A2A2A"))
+        self.best_of_label.setText("")
+        self.best_of_label.adjustSize()
+        self.recalc_sizes()
+
     @pyqtSlot(float)
     def set_cursor_position(self, position: float) -> None:
         self.cursor_position = position
+        if self.global_results is not None:
+            position *= self.mf.audio_player.waveform[:, 0].size
+            genre: int = self.global_results[int(position / self.mf.audio_player.sample_rate / 3)]
+            self.current_genre = self.genre_dict[genre]
+            if self.eq.auto_eq:
+                gains: np.ndarray = self.genre_eq[genre]
+                gains = (gains * 200).astype(np.uint8)
+                self.eq.set_sliders(gains.tolist())
         self.update()
 
 

src/core/audio/AudioStreamer_class.py
@@ -8,7 +8,7 @@ import wavio
 import sounddevice as sd
 
 from PyQt6 import QtCore
-from PyQt6.QtCore import QObject, QThread
+from PyQt6.QtCore import QObject, QThread, pyqtSlot
 
 from src.core.log_system import print_d
 from src.enums import PlayerState
@@ -33,7 +33,8 @@ class AudioStreamer(QThread):
         self._channels: int = 2
         self._volume: float = 1.0
         self.log_volume: bool = True
-        self.eq_gains: List[float] = [1.0 for _ in range(10)]
+        self.eq_active: bool = True
+        self.eq_gains: List[float] = [1.0 for _ in range(20)]
         self.bands: List[tuple] = []
 
         self.pyaudio_port: pyaudio.PyAudio = pyaudio.PyAudio()
@@ -54,7 +55,7 @@ class AudioStreamer(QThread):
                                                      channels=self._channels,
                                                      rate=int(self.sample_rate),
                                                      output=True)
-        self.print_all_devices()
+        # self.print_all_devices()
 
     def close_audio_port(self) -> None:
         self.pyaudio_port.terminate()
@@ -69,11 +70,10 @@ class AudioStreamer(QThread):
                 wave_type = wave_crop.dtype
                 wave_crop = wave_crop.astype(np.float32) / np.iinfo(wave_type).max
                 # region EQ
-                # bands = [(20, 40), (40, 80), (80, 160), (160, 300), (300, 600), (600, 1200), (1200, 2400),
-                #          (2400, 5000), (5000, 10000), (10000, 20000)]
-                wave_crop = equalizer_librosa(wave_crop, self.sample_rate, self.eq_gains, self.bands)
+                if self.eq_active:
+                    wave_crop = equalizer_librosa(wave_crop, self.sample_rate, self.eq_gains, self.bands, n_fft=2048)
+                    wave_crop = np.clip(wave_crop, -1.0, 1.0)
                 wave_crop = (wave_crop * self._volume)
-                wave_crop = np.clip(wave_crop, -1.0, 1.0)
                 wave_crop = (wave_crop * np.iinfo(wave_type).max).astype(wave_type)
                 # endregion
                 if wave_crop.size == 0:
@@ -115,12 +115,12 @@ class AudioStreamer(QThread):
     def duration(self) -> int:
         return self._duration
 
+    @pyqtSlot(bool)
+    def set_eq_active(self, eq_active: bool) -> None:
+        self.eq_active = eq_active
+
     def print_all_devices(self):
         print_d(sd.query_devices())
-        # for i in range(self.pyaudio_port.get_device_count()):
-        #     device_info = self.pyaudio_port.get_device_info_by_index(i)
-        #     # print_d(device_info)
-        #     if device_info['maxOutputChannels'] > 0:
-        #         print_d(f"Device {i}: {device_info['name']}")
+
 
 

src/core/workers/GenrePredictWorker_class.py
@@ -54,7 +54,7 @@ class GenrePredictWorker(QObject):
             for step in range(sample_len):
                 self.preloader_signal.emit(f"Классификация жанра {round(step / sample_len * 100)}%")
                 waveform_part = waveform[step * sample_rate * self.pattern_length:
-                                         (step + 1) * sample_rate * self.pattern_length]
+                                         (step + 1) * sample_rate * self.pattern_length:2]
 
                 input_data = get_genre_input_data(waveform_part, sample_rate)
                 input_data = np.array(input_data)